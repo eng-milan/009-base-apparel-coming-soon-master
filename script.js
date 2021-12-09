38e0cba49496ea7020701edd6b2e8aa0e9680c03
eng-milan <tohid_d.milan@yahoo.com>
2021-12-09 15:29:50 -0600

    email validation modified

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+// document.getElementById("email-submit-btn").addEventListener("click", function () {
+//     const emailInput = document.querySelector(".email-input");
+//     const alertIcon = document.querySelector(".fa-exclamation-circle");
+//     const alertMessage = document.querySelector(".alert-message");
+//     if (emailInput.value.match("@") === null) {
+//         alertIcon.classList.add("alert-icon")
+//         alertMessage.classList.add("alert-message-visible")
+//     } else {
+//         alertIcon.classList.remove("alert-icon")
+//         alertMessage.classList.remove("alert-message-visible")
+//     }
+// })
+
 document.getElementById("email-submit-btn").addEventListener("click", function () {
-    const alertIcon = document.querySelector(".fa-exclamation-circle");
     const emailInput = document.querySelector(".email-input");
+    const alertIcon = document.querySelector(".fa-exclamation-circle");
     const alertMessage = document.querySelector(".alert-message");
-    if (emailInput.value.match("@") === null) {
+    let regExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (regExp.test(emailInput.value) === false) {
         alertIcon.classList.add("alert-icon")
         alertMessage.classList.add("alert-message-visible")
     } else {
         alertIcon.classList.remove("alert-icon")
         alertMessage.classList.remove("alert-message-visible")
     }
+    console.log(regExp.test(emailInput.value));
 })
