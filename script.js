4a477a09cc3b2465f2f09604ffef5e7dc5503590
eng-milan <tohid_d.milan@yahoo.com>
2021-12-10 09:17:32 -0600

    delets extra code comments

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// document.getElementById("email-submit-btn").addEventListener("click", function () {
-//     const emailInput = document.querySelector(".email-input");
-//     const alertIcon = document.querySelector(".fa-exclamation-circle");
-//     const alertMessage = document.querySelector(".alert-message");
-//     if (emailInput.value.match("@") === null) {
-//         alertIcon.classList.add("alert-icon")
-//         alertMessage.classList.add("alert-message-visible")
-//     } else {
-//         alertIcon.classList.remove("alert-icon")
-//         alertMessage.classList.remove("alert-message-visible")
-//     }
-// })
-
 document.getElementById("email-submit-btn").addEventListener("click", function () {
     const emailInput = document.querySelector(".email-input");
     const alertIcon = document.querySelector(".fa-exclamation-circle");
